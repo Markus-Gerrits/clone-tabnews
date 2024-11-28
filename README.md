f45cfb9c507ba9cd72a0e8c29fe9a0559d50ec85
Markus Kaul <markus.gerrits@gmail.com>
2024-11-28 02:36:35 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório com o objetivo de clonar o https://tabnews.com.br para ter a experiência de criar um projeto do zero com a ajuda do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te amo fofinha! 3 anos e 10 meses junto S2</h1>
+  return <h1>Te amo fofinha! 3 anos e 10 meses junto S2</h1>;
 }
 
 export default Home;
